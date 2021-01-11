de32743de453598522e24f04eb3bb79b72b1f847
Prashant-rex <62587657+Prashant-rex@users.noreply.github.com>
2021-01-11 20:32:57 +0530

    Update IP_Tracer.py

--- IP Tracer/IP_Tracer.py
+++ IP Tracer/IP_Tracer.py
@@ -1,25 +1,45 @@
-#!/usr/bin/env/python
+#!/usr/bin/env python3
 
 import requests
 import json
 
-print("[+] Enter the IP Address or name of Website you want to know details of.")
-ip = input("[+] ")
-print("")
+class Tracer():
 
-url = "http://ip-api.com/json/" + ip  #combining the provided ip with api to form the full url.
+    def get_domain(self):
+        print("[+] Enter the IP Address or name of Website you want to know details of.")
+        self.ip = input("[+] ")
+        return self.ip
 
-#http://ip-api/json/192.168.0.1
+    def make_full_url(self):
+        self.url = "http://ip-api.com/json/" + self.ip  #combining the provided ip with api to form the full url.
 
-response = requests.get(url) #sending get request to the url.
-data = json.loads(response.content)  #getting the content of the json file.
+    def get_data(self):
+        self.response = requests.get(self.url) #sending get request to the url.
+        #http://ip-api/json/192.168.0.1
+        self.data = json.loads(self.response.content)  #getting the content of the json file.
     
-print("\t[+] IP       \t\t", data["query"])
-print("\t[+] CITY     \t\t", data["city"])
-print("\t[+] ISP      \t\t", data["isp"])
-print("\t[+] COUNTRY  \t\t", data["country"])
-print("\t[+] REGION   \t\t", data["regionName"])
-print("\t[+] TIME     \t\t", data["timezone"])
-print("\t[+] ZIP      \t\t", data["zip"])
-print("\t[+] LATITUDE \t\t", data["lat"])
-print("\t[+] LOMGITUDE\t\t", data["lon"])
+    def present_data(self): # presenting data
+        print("")
+        print("\t[+] IP       \t\t", self.data["query"])
+        print("\t[+] CITY     \t\t", self.data["city"])
+        print("\t[+] ISP      \t\t", self.data["isp"])
+        print("\t[+] COUNTRY  \t\t", self.data["country"])
+        print("\t[+] REGION   \t\t", self.data["regionName"])
+        print("\t[+] TIME     \t\t", self.data["timezone"])
+        print("\t[+] ZIP      \t\t", self.data["zip"])
+        print("\t[+] LATITUDE \t\t", self.data["lat"])
+        print("\t[+] LOMGITUDE\t\t", self.data["lon"])
+    
+    def __init__(self):
+        self.get_domain()
+        self.make_full_url()
+        self.get_data()
+        self.present_data()
+
+if __name__ == "__main__":
+    
+    try:
+        T = Tracer()
+    except:
+        print("\n[-] Please enter a valid Public IP or Domain.\n")
+    
