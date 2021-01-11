cb00df19c5fbbbe54108047bccaca782654913ae
Prashant-rex <62587657+Prashant-rex@users.noreply.github.com>
2021-01-11 16:38:58 +0530

    Update File_Downloader.py

--- File Downloader/File_Downloader.py
+++ File Downloader/File_Downloader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import requests  #importing the requests library
 
 class Downloader:
@@ -15,14 +15,27 @@ class Downloader:
         with open(file_name, "wb") as out_file: #creating a file name with the same name as original with extension and openning it for write as binary.
             out_file.write(get_response.content) # writng the contents of the get requests to the file created.
 
+    def get_link(self): # function to get the link from the user.
+        print("[+] Enter the absolute link to file.")
+        self.file_to_get = input("[+] ")
+        return self.file_to_get
+    
+    def complete_prompt(self): # funtion to prompt the download complete.
+        print("[+] Downloading file.......")
+        print("[+] Downloaded file as " + self.get_file_name(self.file_to_get))
     
 if __name__ == "__main__":
-
-    print("[+] Enter the absolute link to file.")
-    file_to_get = input("[+] ")
-    D = Downloader()
-    D.download(file_to_get)
-    print("[+] Downloading file.......")
-    print("[+] Downloaded file as " + D.get_file_name(file_to_get))
+  
+    try:
+        D = Downloader()
+    
+        link = D.get_link()
+        D.download(link)
+        D.complete_prompt()
+    
+    except requests.exceptions.MissingSchema:  # handling if user input provided is is not a VALID URL.
+        print("[-] Please provide a valid URL.")
+    except requests.exceptions.ConnectionError: # handling if the file does not exist
+        print("[-] File not found.")
 
 
